18dbcd018d1ce91d105d9ebd2c517ea51625f2c5
Felipe nunes <miotarestaurante2003@gmail.com>
2022-01-25 16:00:24 -0300

    Requisito 1 - Done

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
+const { species } = data
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) {
+    return [];
+  }
+  if (ids.length === 1) {
+    const animal = species.filter(i => i.id === ids[0]);
+    return animal;
+  }
+const array = [];
+ids.forEach((id) => array.push(species.find((i) => i.id === id)));
+return array;
 }
-
 module.exports = getSpeciesByIds;
