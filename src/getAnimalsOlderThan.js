b717bea548ac5dd868a96c5bd8d2219d824121fd
Felipe nunes <miotarestaurante2003@gmail.com>
2022-01-25 17:19:02 -0300

    Requisito 2 - Done

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-}
+const { species } = data;
 
+function getAnimalsOlderThan(animal, age) {
+  const result = species.filter((specie) => specie.name === animal)[0].residents
+    .every((resident) => resident.age > 7);
+  return result;
+}
 module.exports = getAnimalsOlderThan;
