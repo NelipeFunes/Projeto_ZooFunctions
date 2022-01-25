97a8d82896ecda3047ba6da5254dd09ababa9077
Felipe nunes <miotarestaurante2003@gmail.com>
2022-01-25 16:06:14 -0300

    Resoluçao Lint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,5 +1,6 @@
 const data = require('../data/zoo_data');
-const { species } = data
+
+const { species } = data;
 
 function getSpeciesByIds(...ids) {
   if (ids.length === 0) {
@@ -8,9 +9,9 @@ function getSpeciesByIds(...ids) {
   if (ids.length === 1) {
     const animal = species.filter(i => i.id === ids[0]);
     return animal;
-  }
-const array = [];
-ids.forEach((id) => array.push(species.find((i) => i.id === id)));
-return array;
+    }
+  const array = [];
+  ids.forEach((id) => array.push(species.find((i) => i.id === id)));
+  return array;
 }
 module.exports = getSpeciesByIds;
