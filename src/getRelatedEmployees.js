aa2f0c202fcb7d9ea5640ee506b11f0953babe7f
Felipe nunes <miotarestaurante2003@gmail.com>
2022-01-25 19:31:45 -0300

    Requisito 4 - Done

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  const employe = employees.some((employeer) => employeer.managers.includes(id));
+  return employe;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const names = [];
+  employees.forEach((employeer) => {
+    if (employeer.managers.includes(managerId)) {
+      names.push(`${employeer.firstName} ${employeer.lastName}`);
+    }
+  });
+  return names;
 }
 
 module.exports = { isManager, getRelatedEmployees };
