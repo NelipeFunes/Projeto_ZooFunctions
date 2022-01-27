64c9b410e6c506ccc84f16d99d523eb83c3f9988
Felipe nunes <miotarestaurante2003@gmail.com>
2022-01-27 18:09:07 -0300

    Requisito 6 - Done

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,42 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
 function countEntrants(entrants) {
-  // seu código aqui
+  const people = {
+    child: 0,
+    adult: 0,
+    senior: 0,
+  };
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) {
+      people.child += 1;
+    } else if (entrant.age >= 18 && entrant.age < 50) {
+      people.adult += 1;
+    } else {
+      people.senior += 1;
+    }
+  });
+  return people;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  // https://www.samanthaming.com/tidbits/94-how-to-check-if-object-is-empty/
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+  const infos = countEntrants(entrants);
+  return Object.keys(infos).reduce((acc, crr) => acc + (infos[crr] * prices[crr]), 0);
 }
 
 module.exports = { calculateEntry, countEntrants };
+
+/* let cost = 0;
+entrants.forEach((entrant) => {
+  if (entrant.age < 18) {
+    cost += prices.child;
+  } else if (entrant.age >= 18 && entrant.age < 50) {
+    cost += prices.adult;
+  } else {
+    cost += prices.senior;
+  }
+});
+return parseFloat(cost.toFixed(2));
+*/
