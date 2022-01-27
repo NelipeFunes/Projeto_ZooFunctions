0c0f62daababffd666d694c31ee287328954e62c
Felipe nunes <miotarestaurante2003@gmail.com>
2022-01-27 15:27:10 -0300

    Requisito 5 -Done

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  const obj = {};
+  if (!animal) {
+    species.forEach((specie) => {
+      obj[specie.name] = specie.residents.length;
+    });
+    return obj;
+  }
+  const find = species.find((specie) => specie.name === animal.specie);
+  console.log(find);
+  if (animal.sex) {
+    return find.residents.filter((specie) => specie.sex === animal.sex).length;
+  }
+  return species.find((specie) => specie.name === animal.specie).residents.length;
 }
+console.log(countAnimals({ specie: 'giraffes', sex: 'female' }));
 
 module.exports = countAnimals;
