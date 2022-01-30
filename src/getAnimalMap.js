5119afdb9005879a3680e8ee83d204652ed079c0
Felipe Nunes <miotarestaurante2003@gmail.com>
2022-01-30 14:16:57 -0300

    Requsito 7.1 - Done

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,30 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+const { species } = data;
+
+function noParam() {
+  const temp = {
+    NE: [],
+    NW: [],
+    SE: [],
+    SW: [],
+  };
+  const animalsNE = species.filter((specie) => specie.location === 'NE');
+  animalsNE.forEach((animal) => temp.NE.push(animal.name));
+  const animalsNW = species.filter((specie) => specie.location === 'NW');
+  animalsNW.forEach((animal) => temp.NW.push(animal.name));
+  const animalsSE = species.filter((specie) => specie.location === 'SE');
+  animalsSE.forEach((animal) => temp.SE.push(animal.name));
+  const animalsSW = species.filter((specie) => specie.location === 'SW');
+  animalsSW.forEach((animal) => temp.SW.push(animal.name));
+  return temp;
 }
+function getAnimalMap(options) {
+  if (!options) {
+    return noParam();
+  }
+}
+
+console.log(getAnimalMap());
 
 module.exports = getAnimalMap;
