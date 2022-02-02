5b889005e6c4bcf715f1583a801a1c79b4033f71
Felipe Nunes <miotarestaurante2003@gmail.com>
2022-02-02 18:23:48 -0300

    Requisito 8 - Done - Comentado

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -33,34 +33,5 @@ function getSchedule(scheduleTarget) {
 
 module.exports = getSchedule;
 
-// const days = Object.keys(hours);
-// const nameAnimals = species.map((specie) => specie.name);
-
-// function getDay(day) {
-//   let mensage = `Open from ${hours[day].open}am until ${hours[day].close}pm`;
-//   let animals = species.filter((specie) => specie.availability
-//     .includes(day)).map((each) => each.name);
-//   if (day === 'Monday') {
-//     mensage = 'CLOSED';
-//     animals = 'The zoo will be closed!';
-//   }
-//   return {
-//     [day]: {
-//       officeHour: mensage,
-//       exhibition: animals,
-//     },
-//   };
-// }
-// function getSchedule(scheduleTarget) {
-//   if (days.includes(scheduleTarget)) {
-//     return getDay(scheduleTarget);
-//   }
-//   if (nameAnimals.includes(scheduleTarget)) {
-//     return species.find((specie) => specie.name === scheduleTarget).availability;
-//   }
-//   return days.reduce((acc, curr) => ({ ...acc, ...getDay(curr) }), {});
-// }
-
-// module.exports = getSchedule;
-// // codigo feito com base no  do amiguinho KauaASB
-// // https://github.com/tryber/sd-19-b-project-zoo-functions/blob/kaue-alves-zoo-functions-project/src/getSchedule.js
+// codigo feito com base no  do amiguinho KauaASB
+// https://github.com/tryber/sd-19-b-project-zoo-functions/blob/kaue-alves-zoo-functions-project/src/getSchedule.js
